496ad1b53e3632c494b42b42e256649635fd781b
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-08-03 15:45:51 -0500

    fetch data util

--- src/components/RepertoireComponent.vue
+++ src/components/RepertoireComponent.vue
@@ -4,12 +4,23 @@
 
 <script>
 export default {
-  name: "RepertoireComponnet",
+  data() {
+    return {
+      name: "RepertoireComponnet",
+      device: "mobile",
+      html_code: [],
+    };
+  },
   created() {
     this.readHtmlFiles();
   },
   methods: {
-    readHtmlFiles() {},
+    readHtmlFiles() {
+      const domain = `http://127.0.0.1:5000/repertoire?device=${this.device}`;
+      fetch(domain)
+        .then((data) => data.json())
+        .then((data) => this.html_code.push(...data));
+    },
   },
 };
 </script>

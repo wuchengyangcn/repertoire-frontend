1ecb82f9a74cfadebab5592981545214f8862600
Chengyang Wu <wuchengyang.cn@gmail.com>
2023-09-18 00:38:28 -0500

    remove flag

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo docker build -q -t musicnbrain/repertoire-frontend .
+sudo docker build -t musicnbrain/repertoire-frontend .
 sudo docker run -d -p 80:80 --name repertoire-frontend musicnbrain/repertoire-frontend
